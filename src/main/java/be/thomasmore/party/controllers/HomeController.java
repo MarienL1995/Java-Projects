aa68fa6510913a8a608b019b495cbac1083aa64b
MarienL1995 <lander.marien@vdabcampus.be>
2021-02-23 09:13:39 +0100

    correctie

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,45 +1,38 @@
 package be.thomasmore.party.controllers;
-
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.util.Date;
-
 @Controller
-
 public class HomeController {
-    private final int mySpecialNumber = 729;
-    private Date date = new Date();
-    private SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-    private LocalDateTime date2 = LocalDateTime.now().plusDays(30);
-    private final String[] venueNames = {"De Loods", "De Club", "De Hanger", "Zapoi","Kuub","Cuba Libre"};
-    @GetMapping({"/","/home"})
-    public String home(Model model){
-        model.addAttribute("mySpecialNumber",mySpecialNumber);
+    private final String[] venueNames = {"De Loods", "De Club", "De Hangar", "Zapoi", "Kuub", "Cuba Libre"};
+
+    @GetMapping({"/", "/home"})
+    public String home(Model model) {
         return "Home";
     }
+
     @GetMapping("/about")
-    public String about(){
-        return "About";
+    public String about(Model model) {
+        return "about";
     }
-    @GetMapping("/pay")
-    public String pay(Model model){
-        model.addAttribute("Huidedatumentijd",formatter.format(date));
-        model.addAttribute("nieuweTijd",date2);
-        return "pay";
-    }
-    @GetMapping({"/venuedetails","/venuedetails/{venueName}"})
-    public String venueDetails(Model model , @PathVariable(required = false) String venueName){
-        model.addAttribute("venueName",venueName);
+
+    @GetMapping({"/venuedetails", "/venuedetails/{index}"})
+    public String venueDetails(Model model,
+                               @PathVariable(required = false)  Integer index) {
+        if (index!=null && index>=0 && index<venueNames.length ) {
+            model.addAttribute("venueName", venueNames[index]);
+            model.addAttribute("prevIndex", index>0 ? index-1 : venueNames.length-1);
+            model.addAttribute("nextIndex", index<venueNames.length-1 ? index+1 : 0);
+        }
         return "venuedetails";
     }
+
     @GetMapping("/venuelist")
-        public String venueList(Model model){
-        model.addAttribute("venueNames",venueNames);
+    public String venueList(Model model) {
+        model.addAttribute("venueNames", venueNames);
         return "venuelist";
     }
 }
